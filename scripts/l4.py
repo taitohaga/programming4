bb85242231b00dfac2482f98ca5ed57eb78e16c3
taitohaga <s5019223@st.shiga-u.ac.jp>
2021-04-29 00:38:46 +0900

    l4 scripts修正

--- scripts/l4.py
+++ scripts/l4.py
@@ -9,19 +9,5 @@ def add_commit(repo, files=["."], message="何かを修正"):
 	repo.index.add(files)
 	repo.index.commit(message)
 
-def make_key_sh():
-	ssh_key_path = os.path.join(str(Path.home()), ".ssh", "id_rsa")
-	with open("../../id_rsa_tmp", "w") as f:
-		with open(ssh_key_path) as k:
-			key = k.read()
-		f.write(key)
-		os.chmod("../id_rsa_tmp", 0o600)
-	with open("../ssh_cmd.sh", "w") as f:
-		f.write("ssh -T -i ./id_rsa_tmp -o git@github.com")
-		os.chmod("../ssh_cmd.sh", 0o777)
-
 def push(repo):
-	make_key_sh()
-	ssh_executable = "../ssh_cmd.sh"
-	with repo.git.custom_environment(GIT_SSH=ssh_executable):
-		repo.remote().push("main")
+	repo.remote().push("main")
